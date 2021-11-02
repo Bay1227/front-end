2750a5cec4ebd2c4c4cb68e2301365c8049132a2
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-11-02 09:35:20 -0700

    secont push

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  // const development = 'http://localhost:3000'
+  // const production = 'https://phase-5-back.herokuapp.com/'
+  // const url = process.env.NODE_ENV !== 'production' ? production : development;
   const [notes, setNotes] = useState([]);
 
   useEffect(()=> 
-    fetch('http://localhost:3000/notes')
+  fetch('http://localhost:3000/notes')
+    // fetch(`${url}/notes`)       // ('http://localhost:3000/notes')
     .then((res) => res.json())
     .then((json) => setNotes(json)),
     []
